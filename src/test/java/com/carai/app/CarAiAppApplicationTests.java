b18ec0259b6954230bc36e1752354e4fbdf64f97
Dharavath Yamuna <dharavathyamuna15@gmail.com>
2026-03-31 22:57:27 +0530

    Added HomeController

---
+++ src/test/java/com/carai/app/CarAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carai.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
